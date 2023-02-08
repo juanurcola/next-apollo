274972b9ee9690168998319249eceb534806d137
Juan Urcola <juanig.urcola@gmail.com>
2023-02-08 17:48:35 -0300

    Add Link redirect to static pages

--- client/lib/apollo.ts
+++ client/lib/apollo.ts
@@ -1,4 +1,15 @@
-import {ApolloClient, HttpLink, InMemoryCache} from "@apollo/client";
+import {ApolloClient, HttpLink, InMemoryCache, from} from "@apollo/client";
+import {onError} from "@apollo/client/link/error";
+
+const errorLink = onError(({graphQLErrors, networkError}) => {
+  if (graphQLErrors) {
+    console.log(graphQLErrors);
+  }
+
+  if (networkError) {
+    console.log(networkError);
+  }
+});
 
 const httpLink = new HttpLink({
   uri: `${process.env.NEXT_PUBLIC_URL_SITE}/api/graphql`,
@@ -6,7 +17,7 @@ const httpLink = new HttpLink({
 });
 
 export const client = new ApolloClient({
-  link: httpLink,
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache({
     addTypename: false,
   }),
